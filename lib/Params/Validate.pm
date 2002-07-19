49c500bb2ef4c5ce1d769f021f99d152db5ab13d
Dave Rolsky <autarch@urth.org>
2002-07-19 04:09:00 +0000

    Import of DROLSKY/Params-Validate-0.24 from CPAN.
    gitpan-cpan-distribution: Params-Validate
    gitpan-cpan-version:      0.24
    gitpan-cpan-path:         DROLSKY/Params-Validate-0.24.tar.gz
    gitpan-cpan-author:       DROLSKY
    gitpan-cpan-maturity:     released

--- lib/Params/Validate.pm
+++ lib/Params/Validate.pm
@@ -42,7 +42,7 @@ my %tags =
 @EXPORT_OK = ( @{ $EXPORT_TAGS{all} }, 'set_options' );
 @EXPORT = qw( validate validate_pos );
 
-$VERSION = '0.23';
+$VERSION = '0.24';
 
 =pod
 
@@ -232,7 +232,7 @@ sub validate (\@$)
                           unless exists $ref->{$_};
                   }
 
-                  $ref;
+                  return $ref;
               }
             );
     }
